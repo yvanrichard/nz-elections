321dc19d87eb65614a813fd7399e3694acaff1a9
Yvan Richard <yvan@dragonfly.co.nz>
2017-09-21 17:20:00 +1200

    Shiny app to simulate government

--- shiny/server.r
+++ shiny/server.r
@@ -7,91 +7,126 @@ library(data.table)
 library(nzelect)
 library(shiny)
 library(shinydashboard)
-library(shinyjs)
-library(lubridate)
 library(ggplot2)
-## library(ggthemes)
-library(RColorBrewer)
-library(DT)
-## library(V8)
-## library(viridis)
+library(plotly)
 options(scipen = 100)
 options(browser = 'google-chrome')
 
 
-## source('helpers.r')
-
 evote <- fread('data/electoral-votes_ordinary-special.csv')
 pvote <- fread('data/party-votes_ordinary-special.csv')
+
+getmainparties <- function(parties) {
+    return(ifelse(parties == 'National Party', 'National',
+        ifelse(parties == 'Labour Party', 'Labour',
+        ifelse(parties == 'Green Party', 'Green',
+        ifelse(parties == 'NZ First', 'NZ First',
+        ifelse(parties == 'Maori Party', 'Maori',
+        ifelse(parties == 'TOP', 'TOP',
+        ifelse(parties == 'ACT', 'ACT',
+        ifelse(parties == 'Conservative', 'Conservative',
+        ifelse(parties == 'United Future', 'United Future',
+               'Other'))))))))))
+}
 
 evote14 <- evote[year == 2014,
                 .(party = .SD[which.max(all), party]),
                 .(year, electorate)][, .(elec_won=.N), .(year, party)]
+pvote14 <- pvote[year == 2014,
+                .(votes = sum(all)), party]
+pvote14 <- pvote14[, .(votes = sum(votes)), .(party = getmainparties(party))]
+pvote14 <- rbind(pvote14, data.table(party='TOP', votes=0))
+pvote14[, p := votes / sum(votes)]
+setkey(pvote14, party)
+pvote14['National', w := 100.0]
+pnat <- pvote14['National', p]
+for (i in 1:nrow(pvote14)) {
+    pvote14[i, w := 100.0 * p / pnat]
+}
+
+cols <- c(Green           = '#008641',
+         Labour          = '#D82C20',
+         National        = '#00529E',
+         'NZ First'      = '#555555',
+         TOP             = '#0F175F',
+         Maori           = '#F6A78B',
+         ACT             = '#FFDA37',
+         Conservative    = '#F37133',
+         'United Future' = '#4B104E',
+         Other           = '#999999')
 
 session <- NULL
 debug <- FALSE
 
 enableBookmarking(store = "url")
 
-input <- list(national_w = 47, labour_w = 25.1, green_w = 10.7, nzfirst_w = 8.7, top_w = 0,
-             maori_w = 1.3, act_w = 0.7, other_w = 6.5,
-             national_e = T, labour_e = T, green_e = F, nzfirst_e = F, top_e = F,
-             maori_e = T, act_e = T, other_e = T)
              
 shinyServer(function(input, output, session) {
-    
+   
 
 #################
 ## UI elements ##
 #################
 
     output$national_w <- renderUI({
-        sliderInput("national_w", "", min = 0, max = 60, step = 0.1, value = 47, ticks = F)
+        sliderInput("national_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['National', w], ticks = F)
     })
     output$national_e <- renderUI({
-        checkboxInput("national_e", "", value = T)
+        checkboxInput("national_e", NULL, value = T)
     })
     output$labour_w <- renderUI({
-        sliderInput("labour_w", "", min = 0, max = 60, step = 0.1, value = 25.1, ticks = F)
+        sliderInput("labour_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['Labour', w], ticks = F)
     })
     output$labour_e <- renderUI({
-        checkboxInput("labour_e", "", value = T)
+        checkboxInput("labour_e", NULL, value = T)
     })
     output$green_w <- renderUI({
-        sliderInput("green_w", "", min = 0, max = 60, step = 0.1, value = 10.7, ticks = F)
+        sliderInput("green_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['Green', w], ticks = F)
     })
     output$green_e <- renderUI({
-        checkboxInput("green_e", "", value = F)
+        checkboxInput("green_e", NULL, value = F)
     })
     output$nzfirst_w <- renderUI({
-        sliderInput("nzfirst_w", "", min = 0, max = 60, step = 0.1, value = 8.7, ticks = F)
+        sliderInput("nzfirst_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['NZ First', w], ticks = F)
     })
     output$nzfirst_e <- renderUI({
-        checkboxInput("nzfirst_e", "", value = F)
+        checkboxInput("nzfirst_e", NULL, value = F)
     })
     output$top_w <- renderUI({
-        sliderInput("top_w", "", min = 0, max = 60, step = 0.1, value = 0, ticks = F)
+        sliderInput("top_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['TOP', w], ticks = F)
     })
     output$top_e <- renderUI({
-        checkboxInput("top_e", "", value = F)
+        checkboxInput("top_e", NULL, value = F)
     })
     output$maori_w <- renderUI({
-        sliderInput("maori_w", "", min = 0, max = 60, step = 0.1, value = 1.3, ticks = F)
+        sliderInput("maori_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['Maori', w], ticks = F)
     })
     output$maori_e <- renderUI({
-        checkboxInput("maori_e", "", value = T)
+        checkboxInput("maori_e", NULL, value = T)
     })
     output$act_w <- renderUI({
-        sliderInput("act_w", "", min = 0, max = 60, step = 0.1, value = 0.7, ticks = F)
+        sliderInput("act_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['ACT', w], ticks = F)
     })
     output$act_e <- renderUI({
-        checkboxInput("act_e", "", value = T)
+        checkboxInput("act_e", NULL, value = T)
+    })
+    output$conserv_w <- renderUI({
+        sliderInput("conserv_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['Conservative', w], ticks = F)
+    })
+    output$conserv_e <- renderUI({
+        checkboxInput("conserv_e", NULL, value = F)
+    })
+    output$ufuture_w <- renderUI({
+        sliderInput("ufuture_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['United Future', w], ticks = F)
+    })
+    output$ufuture_e <- renderUI({
+        checkboxInput("ufuture_e", NULL, value = T)
     })
     output$other_w <- renderUI({
-        sliderInput("other_w", "", min = 0, max = 60, step = 0.1, value = 6.5, ticks = F)
+        sliderInput("other_w", NULL, min = 0, max = 100, step = 0.01, value = pvote14['Other', w], ticks = F)
     })
     output$other_e <- renderUI({
-        checkboxInput("other_e", "", value = F)
+        checkboxInput("other_e", NULL, value = F)
     })
 
     
@@ -99,14 +134,15 @@ shinyServer(function(input, output, session) {
 ## Derived data ##
 ##################
 
-    ## Weights of party votes
+    ## Election results
     parties <- reactive({
         if (!is.null(input$other_w)) {
-        d <- data.table(party = c('National', 'Labour', 'Green', 'NZ First', 'TOP', 'Maori', 'ACT', 'Other'),
+            d <- data.table(party = c('National', 'Labour', 'Green', 'NZ First', 'TOP',
+                                     'Maori', 'ACT', 'Conservative', 'United Future', 'Other'),
                        weights = c(input$national_w, input$labour_w, input$green_w, input$nzfirst_w, input$top_w,
-                                   input$maori_w, input$act_w, input$other_w),
+                                   input$maori_w, input$act_w, input$conserv_w, input$ufuture_w, input$other_w),
                        w_elec = c(input$national_e, input$labour_e, input$green_e, input$nzfirst_e, input$top_e,
-                                  input$maori_e, input$act_e, input$other_e))
+                                  input$maori_e, input$act_e, input$conserv_e, input$ufuture_e, input$other_e))
         d[, p := 100 * weights / sum(weights)]
         d[, e := ifelse(w_elec == T, 1, 0)]
         d[, seats := allocate_seats(d$p, d$party, electorate=d$e)$seats_v]
@@ -114,24 +150,79 @@ shinyServer(function(input, output, session) {
         }
     })
 
-    output$table <- renderTable({
+    ## Report back on % of party votes and the number of seats next to controls
+    output$national_p <- renderText({if (!is.null(parties())) {parties()[party == 'National', sprintf('%0.1f %%', p)]}})
+    output$national_s <- renderText({if (!is.null(parties())) {parties()[party == 'National', sprintf('%i seats', seats)]}})
+    output$labour_p <- renderText({if (!is.null(parties())) {parties()[party == 'Labour', sprintf('%0.1f %%', p)]}})
+    output$labour_s <- renderText({if (!is.null(parties())) {parties()[party == 'Labour', sprintf('%i seats', seats)]}})
+    output$green_p <- renderText({if (!is.null(parties())) {parties()[party == 'Green', sprintf('%0.1f %%', p)]}})
+    output$green_s <- renderText({if (!is.null(parties())) {parties()[party == 'Green', sprintf('%i seats', seats)]}})
+    output$nzfirst_p <- renderText({if (!is.null(parties())) {parties()[party == 'NZ First', sprintf('%0.1f %%', p)]}})
+    output$nzfirst_s <- renderText({if (!is.null(parties())) {parties()[party == 'NZ First', sprintf('%i seats', seats)]}})
+    output$top_p <- renderText({if (!is.null(parties())) {parties()[party == 'TOP', sprintf('%0.1f %%', p)]}})
+    output$top_s <- renderText({if (!is.null(parties())) {parties()[party == 'TOP', sprintf('%i seats', seats)]}})
+    output$maori_p <- renderText({if (!is.null(parties())) {parties()[party == 'Maori', sprintf('%0.1f %%', p)]}})
+    output$maori_s <- renderText({if (!is.null(parties())) {parties()[party == 'Maori', sprintf('%i seats', seats)]}})
+    output$act_p <- renderText({if (!is.null(parties())) {parties()[party == 'ACT', sprintf('%0.1f %%', p)]}})
+    output$act_s <- renderText({if (!is.null(parties())) {parties()[party == 'ACT', sprintf('%i seats', seats)]}})
+    output$conserv_p <- renderText({if (!is.null(parties())) {parties()[party == 'Conservative', sprintf('%0.1f %%', p)]}})
+    output$conserv_s <- renderText({if (!is.null(parties())) {parties()[party == 'Conservative', sprintf('%i seats', seats)]}})
+    output$ufuture_p <- renderText({if (!is.null(parties())) {parties()[party == 'United Future', sprintf('%0.1f %%', p)]}})
+    output$ufuture_s <- renderText({if (!is.null(parties())) {parties()[party == 'United Future', sprintf('%i seats', seats)]}})
+    output$other_p <- renderText({if (!is.null(parties())) {parties()[party == 'Other', sprintf('%0.1f %%', p)]}})
+    output$other_s <- renderText({if (!is.null(parties())) {parties()[party == 'Other', sprintf('%i seats', seats)]}})
+
+
+    ## Government pie
+    output$pie  <- renderPlotly({
         d <- parties()
-        return(d)
+        if (!is.null(d)) {
+
+                clrs <- cols[d$party]
+
+                p <- plot_ly(d, labels = ~party, values = ~seats, type = 'pie',
+                            textposition = 'inside',
+                            textinfo = 'label+value',
+                            hoverinfo = 'text',
+                            insidetextfont = list(color = '#FFFFFF'),
+                            text = ~sprintf('%s\n%0.1f %% party votes\n%i seats', party, p, seats),
+                            marker = list(colors = clrs),
+                            showlegend = F,
+                            dislayModeBar = F
+                            ) %>%
+                    layout(title = 'Seats in NZ Parliament',
+                           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+                p %>% config(displayModeBar = F)
+
+            } else return(NULL)
     })
 
-    
-    
-###########
-## Plots ##
-###########
-
-    ## output$plot <- renderPlot({
-    ## })
-    
-    ## output$zelog <- renderPrint({
-    ##     print()
-    ## })
-
+    ## Table of potential winning coalitions
+    output$coalitions <- renderTable({
+        d <- parties()
+        if (!is.null(d)) {
+            ord <- d[order(p), party]
+            minseats <- sum(d$seats) / 2
+            pairs <- CJ(party1 = d$party, party2 = d$party)
+            pairs <- pairs[party1 != party2]
+            pairs <- pairs[as.numeric(factor(party1, levels = ord)) > as.numeric(factor(party2, levels = ord))]
+            pairs[, seats1 := d[match(party1, party), seats]]
+            pairs[, seats2 := d[match(party2, party), seats]]
+            pairs[, tot := seats1 + seats2]
+            wins <- pairs[tot >= minseats]
+            wins <- wins[!(party1 %in% c('National', 'Labour') & party2 %in% c('National', 'Labour'))]
+            setorder(wins, -tot)
+            maxchar1 <- wins[, max(nchar(party1))]
+            maxchar2 <- wins[, max(nchar(party2))]
+            wins[, lab := sprintf(sprintf('%%%is - %%%is', maxchar1, maxchar2), party1, party2)]
+            wins[, seats := sprintf('%i seats', tot)]
+            if (nrow(wins)) {
+                return(wins[, .(lab, seats)])
+            }
+        }
+    }, colnames = F)
     
 
 })
--- shiny/ui.r
+++ shiny/ui.r
@@ -4,95 +4,120 @@
 
 library(data.table)
 library(shiny)
-library(shinyjs)
+## library(shinyjs)
 library(shinydashboard)
-library(DT)
-library(V8)
+library(plotly)
+## library(DT)
+## library(V8)
 ## library(rbokeh)
 addResourcePath('data', 'data')
 
 enableBookmarking(store = "url")
 
 function(request) {
-    ## loadingLogo <- function(href, src, loadingsrc, height = NULL, width = NULL, alt = NULL) {
-    ##     ## tagList(
-    ##     ##     tags$head(
-    ##     ##              tags$script(
-    ##     ##                       "setInterval(function(){
-    ##     ##              if ($('html').attr('class')=='shiny-busy') {
-    ##     ##              $('div.busy').show();
-    ##     ##              $('div.notbusy').hide();
-    ##     ##              } else {
-    ##     ##              $('div.busy').hide();
-    ##     ##              $('div.notbusy').show();
-    ##     ##    }
-    ##     ##  },100)")
-    ##     ##  ),
-    ##     ##  div(class = "busy",  
-    ##     ##      img(src=loadingsrc,height = height, width = width, alt = alt)),
-    ##     ##  div(class = 'notbusy',
-    ##     ##      div(class = 'logo', "Government simulator"))
-    ##     ## )
-    ## }
+
     dashboardPage(
-        title = "Government simulator",
+        title = "NZ Government Simulator",
         header = dashboardHeader(
-            title = 'Government simulator',
-                     ## title = loadingLogo('http://www.google.co.nz',
-                     ##                     'data/logo.png',
-                     ##                     'data/ajax-loader.gif'),
+            title = 'NZ Government Simulator',
                      titleWidth = 300
                  ),
         
-        sidebar = dashboardSidebar(width = 400,
+        sidebar = dashboardSidebar(width = '0%'
+        ),
+            
+        body = dashboardBody(
             tags$head(
                      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
                  ),
-            useShinyjs(),  # Set up shinyjs
-            fluidRow(column(3, h5('')),
-                     column(3, align = 'center', h4('Importance')),
-                     column(3, uiOutput('Won electorates'))),
-            fluidRow(column(3, h5('National')),
-                     column(7, uiOutput('national_w')),
-                     column(1, uiOutput('national_e'))),
-            fluidRow(column(3, h5('Labour')),
-                     column(7, uiOutput('labour_w')),
-                     column(1, uiOutput('labour_e'))),
-            fluidRow(column(3, h5('Green')),
-                     column(7, uiOutput('green_w')),
-                     column(1, uiOutput('green_e'))),
-            fluidRow(column(3, h5('NZ First')),
-                     column(7, uiOutput('nzfirst_w')),
-                     column(1, uiOutput('nzfirst_e'))),
-            fluidRow(column(3, h5('TOP')),
-                     column(7, uiOutput('top_w')),
-                     column(1, uiOutput('top_e'))),
-            fluidRow(column(3, h5('Maori')),
-                     column(7, uiOutput('maori_w')),
-                     column(1, uiOutput('maori_e'))),
-            fluidRow(column(3, h5('ACT')),
-                     column(7, uiOutput('act_w')),
-                     column(1, uiOutput('act_e'))),
-            fluidRow(column(3, h5('Other')),
-                     column(7, uiOutput('other_w')),
-                     column(1, uiOutput('other_e'))),
-            hr(),
-            bookmarkButton()
-        ),
-        
+            box(width = 12,
+                h3('Find out how many seats each party would have in the New Zealand government'),
+                p('Change the importance sliders to get the desired percentages of party votes'),
+                p('Tick the checkbox if the party won at least one electorate (a party with less than 5% of votes is assumed to win only one electorate)'),
+                p('The numbers are initialised at the results of the 2014 election')
+                ),
+            box(title = 'Election results',
+                width = 4,
+                hr(),
+            fluidRow(column(4, strong('Relative importance')),
+                     column(3, h5('')),
+                     column(1, align='center', strong('Won electo- rates')),
+                     column(2, align='center', strong('Party votes %')),
+                     column(2, align='center', strong('Seats'))),
+            fluidRow(column(4, uiOutput('national_w')),
+                     column(3, align='center', h4('National')),
+                     column(1, align='center', uiOutput('national_e')),
+                     column(2, textOutput('national_p')),
+                     column(2, textOutput('national_s'))),
+            fluidRow(column(4, uiOutput('labour_w')),
+                     column(3, align='center', h4('Labour')),
+                     column(1, align='center', uiOutput('labour_e')),
+                     column(2, textOutput('labour_p')),
+                     column(2, textOutput('labour_s'))),
+            fluidRow(column(4, uiOutput('green_w')),
+                     column(3, align='center', h4('Green')),
+                     column(1, align='center', uiOutput('green_e')),
+                     column(2, textOutput('green_p')),
+                     column(2, textOutput('green_s'))),
+            fluidRow(column(4, uiOutput('nzfirst_w')),
+                     column(3, align='center', h4('NZ First')),
+                     column(1, align='center', uiOutput('nzfirst_e')),
+                     column(2, textOutput('nzfirst_p')),
+                     column(2, textOutput('nzfirst_s'))),
+            fluidRow(column(4, uiOutput('top_w')),
+                     column(3, align='center', h4('TOP')),
+                     column(1, align='center', uiOutput('top_e')),
+                     column(2, textOutput('top_p')),
+                     column(2, textOutput('top_s'))),
+            fluidRow(column(4, uiOutput('maori_w')),
+                     column(3, align='center', h4('Maori')),
+                     column(1, align='center', uiOutput('maori_e')),
+                     column(2, textOutput('maori_p')),
+                     column(2, textOutput('maori_s'))),
+            fluidRow(column(4, uiOutput('conserv_w')),
+                     column(3, align='center', h4('Conservative')),
+                     column(1, align='center', uiOutput('conserv_e')),
+                     column(2, textOutput('conserv_p')),
+                     column(2, textOutput('conserv_s'))),
+            fluidRow(column(4, uiOutput('ufuture_w')),
+                     column(3, align='center', h4('United Future')),
+                     column(1, align='center', uiOutput('ufuture_e')),
+                     column(2, textOutput('ufuture_p')),
+                     column(2, textOutput('ufuture_s'))),
+            fluidRow(column(4, uiOutput('act_w')),
+                     column(3, align='center', h4('ACT')),
+                     column(1, align='center', uiOutput('act_e')),
+                     column(2, textOutput('act_p')),
+                     column(2, textOutput('act_s'))),
+            fluidRow(column(4, uiOutput('other_w')),
+                     column(3, align='center', h4('Other')),
+                     column(1, align='center', uiOutput('other_e')),
+                     column(2, textOutput('other_p')),
+                     column(2, textOutput('other_s')))
+            ),
+                
+            box(title = 'Parliament', width = 4,
+                hr(),
+                plotlyOutput('pie')
+                ),
             
-        body = dashboardBody(
-                   ## navbarPage('>>', id = 'navbar',
-                   ##            tabPanel("Set up", value = 'tab1',
-            ## useShinyjs(),  # Set up shinyjs
-            ## extendShinyjs(text = jscode),
-            ## inlineCSS(css),
-            box(title = 'Parliament', width = 6, tableOutput('table')),
-            box(title = 'Info', width = 6,
-                h5('The importance sliders on the left hand side drive the results of party votes.'),
-                h5('The check box on their right specifies whether the party won any electorate (assumed to be one electorate).'),
-                h5('All calculations are made using the package nzelect from Peter Ellis.'))
-             
+            box(title = 'Potential coalitions', width = 4,
+                hr(),
+                p('List of pairs of parties that could form a majority government'),
+                p('Coalition Labour/National is assumed to be impossible'),
+                tableOutput('coalitions')
+                ),
+
+            box(title = 'Info', width = 4,
+                hr(),
+                p('The relative importance is not a % of party votes. The % is calculated from the ratio of the importance to the sum of all importance values'),
+                HTML('The calculation of the seats allocation is from Peter Ellis\'s <a href="https://github.com/ellisp/nzelect">nzelect</a> R package'),
+                hr(),
+                p('E-mail author: thefrenchstick<at>gmail.com'),
+                p(),
+                HTML('Source code <a href="https://github.com/yvanrichard/nz-elections">on GitHub</a>')
+                
+                )
         )
     )
 }
